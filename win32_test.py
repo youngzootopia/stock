d01e8c9c9568ff1738acd50c6b85410583adfdc5
youngzootopia <youngzootopia@gmail.com>
2023-08-23 16:16:40 +0900

    키움 API 통해서 로그인 창 띄우고, json 설정파일을 통해 자동 로그인 테스트

--- win32_test.py
+++ win32_test.py
@@ -2,7 +2,10 @@ import win32gui
 import win32con
 import win32api
 import time
+import json
 
+with open('./config/pass.json') as f:
+    config = json.load(f)
 
 def window_enumeration_handler(hwnd, top_windows):
     top_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
@@ -25,7 +28,29 @@ def find_window(caption):
     return hwnd
 
 
+def enter_keys(hwnd, data):
+    win32api.SendMessage(hwnd, win32con.EM_SETSEL, 0, -1)
+    win32api.SendMessage(hwnd, win32con.EM_REPLACESEL, 0, data)
+    win32api.Sleep(300)
+
+
+def click_button(btn_hwnd):
+    win32api.PostMessage(btn_hwnd, win32con.WM_LBUTTONDOWN, 0, 0)
+    win32api.Sleep(100)
+    win32api.PostMessage(btn_hwnd, win32con.WM_LBUTTONUP, 0, 0)
+    win32api.Sleep(300)
+
+
 if __name__ == "__main__":
-    caption = "Open API Login" # 키움 Open API
+    caption = "Open API Login"
     hwnd = find_window(caption)
-    print(hwnd)
+
+    #edit_id = win32gui.GetDlgItem(hwnd, 0x3E8)
+    edit_pass = win32gui.GetDlgItem(hwnd, 0x3E9)
+    #edit_cert = win32gui.GetDlgItem(hwnd, 0x3EA)
+    btn_login = win32gui.GetDlgItem(hwnd, 0x1)
+
+    #enter_keys(edit_id, "아이디")
+    enter_keys(edit_pass, config['DEFAULT']['KIWOOM_PASS'])
+    #enter_keys(edit_cert, "인증비밀번호")
+    click_button(btn_login)
