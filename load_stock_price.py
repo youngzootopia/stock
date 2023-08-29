e26886399b5c019cd108db98d22392199008783d
youngzootopia <youngzootopia@gmail.com>
2023-08-29 17:01:59 +0900

    주식 가격정보 적재 타이머 5초로 변경

--- load_stock_price.py
+++ load_stock_price.py
@@ -15,6 +15,7 @@ if __name__ == '__main__': # 중복 방지를 위해 사용
     # kodak_list = Kiwoom.get_code_list_stok_market("10")
 
     for kospi in kospi_list:
+        print(kospi)
         stock_price = Kiwoom.get_price(kospi)
         stock_price.insert(0, "code", kospi)
         Mongo.insert_price_many(stock_price)
