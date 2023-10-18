8d9edfa181cad4269e3fbf7bcbf4aaaad7bf3631
youngzootopia <youngzootopia@gmail.com>
2023-10-18 21:59:23 +0900

    매일 실행할 예정이므로 데일리 로드 today로 인수 변경

--- load_stock_price.py
+++ load_stock_price.py
@@ -17,9 +17,9 @@ def daily_load(start_date):
 @dispatch(str, str)
 def daily_load(start_date, end_date):
     # 종목 정보 가져오기
-    # kospi_list = Kiwoom.get_code_list_stok_market("0")
+    kospi_list = Kiwoom.get_code_list_stok_market("0")
     # kosdak_list = Kiwoom.get_code_list_stok_market("10")
-    kospi_list = []
+    # kospi_list = []
     kospi_list.append("KOSPI")
 
     # 특정 종목부터 받아올 경우 isNext 사용
@@ -146,7 +146,7 @@ if __name__ == '__main__': # 중복 방지를 위해 사용
     Mongo = Mongo()
     Ml_stock = Ml_stock()
 
-    daily_load("20231013")       
+    daily_load(datetime.today().strftime("%Y%m%d"))       
 
     app.exec_()
     
