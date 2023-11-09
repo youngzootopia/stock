741f9e9d4672c6b1dd655c9ff53910f3388db663
youngzootopia <youngzootopia@gmail.com>
2023-11-09 13:33:04 +0900

    주문 체결 시 임의 주문 넣었던가 등의 사유로 키 없을 수 있음

--- kiwoom.py
+++ kiwoom.py
@@ -178,47 +178,50 @@ class Kiwoom(QAxWidget):
     # fid_list FID의 경우 키움API에서 미리 정의된 코드 값
     def _on_receive_chejan(self, gubun, cnt, fid_list):
         # print(gubun)
-        if gubun == "1": # 잔고
-            # 주문가능수량 갱신
-            code = self.dynamicCall("GetChejanData(int)", "9001")[1:] 
-            available_quantity = int(self.dynamicCall("GetChejanData(int)", "933"))
+        try:
+            if gubun == "1": # 잔고
+                # 주문가능수량 갱신
+                code = self.dynamicCall("GetChejanData(int)", "9001")[1:] 
+                available_quantity = int(self.dynamicCall("GetChejanData(int)", "933"))
 
-            self.stock_dict[code]['available_quantity'] = available_quantity
-        elif gubun == "0": # 체결
-            code = self.dynamicCall("GetChejanData(int)", "9001")[1:] 
-            name = self.dynamicCall("GetChejanData(int)", "302").strip() # 종목명
-            division = self.dynamicCall("GetChejanData(int)", "907") # 매도수 구분, 1:매도, 2:매수
-            che = self.dynamicCall("GetChejanData(int)", "911").lstrip("+").lstrip("-") # 체결량
-            price = self.dynamicCall("GetChejanData(int)", "910").lstrip("+").lstrip("-") # 체결가
-            available_quantity = self.dynamicCall("GetChejanData(int)", "930").lstrip("+").lstrip("-") # 보유수량
-            buy_close = self.dynamicCall("GetChejanData(int)", "910").lstrip("+").lstrip("-") # 매입단가
+                self.stock_dict[code]['available_quantity'] = available_quantity
+            elif gubun == "0": # 체결
+                code = self.dynamicCall("GetChejanData(int)", "9001")[1:] 
+                name = self.dynamicCall("GetChejanData(int)", "302").strip() # 종목명
+                division = self.dynamicCall("GetChejanData(int)", "907") # 매도수 구분, 1:매도, 2:매수
+                che = self.dynamicCall("GetChejanData(int)", "911").lstrip("+").lstrip("-") # 체결량
+                price = self.dynamicCall("GetChejanData(int)", "910").lstrip("+").lstrip("-") # 체결가
+                available_quantity = self.dynamicCall("GetChejanData(int)", "930").lstrip("+").lstrip("-") # 보유수량
+                buy_close = self.dynamicCall("GetChejanData(int)", "910").lstrip("+").lstrip("-") # 매입단가
 
-            if che.isdigit() and price.isdigit() and available_quantity.isdigit() and buy_close.isdigit():
-                che = int(che)
-                price = int(price)
-                available_quantity = int(available_quantity)
-                buy_close = int(buy_close)
-            else:
-                che = 0
-                price = 0
-                available_quantity = 0
-                buy_close = 0.0
-            
-            if che > 0 and division == '2': # 매수 체결 시
-                self.stock_dict[code]['available_quantity'] = available_quantity # 보유수량 수정
-                self.stock_dict[code]['buy_close'] = buy_close # 매수가격 수정
+                if che.isdigit() and price.isdigit() and available_quantity.isdigit() and buy_close.isdigit():
+                    che = int(che)
+                    price = int(price)
+                    available_quantity = int(available_quantity)
+                    buy_close = int(buy_close)
+                else:
+                    che = 0
+                    price = 0
+                    available_quantity = 0
+                    buy_close = 0.0
                 
-                self.stock_dict[code]['order_quantity'] = 0 # 주문 체결 시 0으로 초기화 하여야 당일 매수/매도 가능함
-                self.teleBot.report_message("{} 매수체결: {} * {}, 잔고: {} * {} = {}".format(name, price, che, self.stock_dict[code]['buy_close'], self.stock_dict[code]['available_quantity'], self.stock_dict[code]['buy_close'] * self.stock_dict[code]['available_quantity']))
+                if che > 0 and division == '2': # 매수 체결 시
+                    self.stock_dict[code]['available_quantity'] = available_quantity # 보유수량 수정
+                    self.stock_dict[code]['buy_close'] = buy_close # 매수가격 수정
+                    
+                    self.stock_dict[code]['order_quantity'] = 0 # 주문 체결 시 0으로 초기화 하여야 당일 매수/매도 가능함
+                    self.teleBot.report_message("{} 매수체결: {} * {}, 잔고: {} * {} = {}".format(name, price, che, self.stock_dict[code]['buy_close'], self.stock_dict[code]['available_quantity'], self.stock_dict[code]['buy_close'] * self.stock_dict[code]['available_quantity']))
 
-            if che > 0 and division == '1': # 매도 체결 시
-                self.stock_dict[code]['available_quantity'] = available_quantity # 보유수량 수정
-                self.stock_dict[code]['buy_close'] = buy_close # 매수가격 수정
+                if che > 0 and division == '1': # 매도 체결 시
+                    self.stock_dict[code]['available_quantity'] = available_quantity # 보유수량 수정
+                    self.stock_dict[code]['buy_close'] = buy_close # 매수가격 수정
 
-                self.stock_dict[code]['order_quantity'] = 0
-                self.teleBot.report_message("{} 매도체결: {} * {}, 잔고: {} * {}, {}%".format(name, price, che, self.stock_dict[code]['buy_close'], self.stock_dict[code]['available_quantity'], (price - self.stock_dict[code]['buy_close']) / self.stock_dict[code]['buy_close'] * 100))
+                    self.stock_dict[code]['order_quantity'] = 0
+                    self.teleBot.report_message("{} 매도체결: {} * {}, 잔고: {} * {}, {}%".format(name, price, che, self.stock_dict[code]['buy_close'], self.stock_dict[code]['available_quantity'], (price - self.stock_dict[code]['buy_close']) / self.stock_dict[code]['buy_close'] * 100))
+        except Exception as e:
+            print(e)
 
-        '''
+        
         for fid in fid_list.split(";"):
             # FID 9001 = 종목 코드, 결과의 경우 문자+종목코드 이므로 문자 제외하고 슬라이싱
             data = self.dynamicCall("GetChejanData(int)", fid).lstrip("+").lstrip("-")
@@ -237,7 +240,7 @@ class Kiwoom(QAxWidget):
                 print("{} : {}".format(name, data))
             except KeyError:
                 print("FID {} 는 정의되지 않았습니다.".format(fid))
-        '''
+        
         
             
 
